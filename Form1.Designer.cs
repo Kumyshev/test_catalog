b51d24c0128dfa54308d15924d3076d8f6409313
Kumyshev <darklordsithahmed@gmail.com>
2019-11-25 20:13:18 +0300

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace appForJob4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 75);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(284, 359);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(302, 75);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(464, 329);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(302, 410);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 24);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "НАЗАД";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(386, 410);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 24);
+            this.btnForward.TabIndex = 4;
+            this.btnForward.Text = "ВПЕРЕД";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(12, 43);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(534, 22);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(552, 41);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 24);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "ПОИСК";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(691, 410);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 24);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "ВЫХОД";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "*.txt"});
+            this.comboBox1.Location = new System.Drawing.Point(645, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(807, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 324);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 453);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace appForJob4
+{
+    public partial class Form1 : Form
+    {
+
+
+        string FULL_PATH;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetDrive();
+        }
+
+        public void GetDirect(TreeNode treeDrive)
+        {
+            DirectoryInfo[] directs;
+            treeDrive.Nodes.Clear();
+            string fullPath = treeDrive.FullPath;
+            DirectoryInfo direct = new DirectoryInfo(fullPath);
+
+            try
+            {
+                directs = direct.GetDirectories();
+            }
+            catch
+            {
+                return;
+            }
+            foreach (DirectoryInfo dirinfo in directs)
+            {
+                TreeNode dir = new TreeNode(dirinfo.Name, 0, 0);
+                treeDrive.Nodes.Add(dir);
+            }
+        }
+
+
+        public void GetDrive()
+        {
+            string[] drives = Directory.GetLogicalDrives();
+
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Clear();
+
+            foreach (string drive in drives)
+            {
+                TreeNode treeDrive = new TreeNode(drive, 0, 0);
+                treeView1.Nodes.Add(treeDrive);
+
+                GetDirect(treeDrive);
+            }
+            treeView1.EndUpdate();
+        }
+
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView1.BeginUpdate();
+            foreach (TreeNode treeNode in e.Node.Nodes)
+            {
+                GetDirect(treeNode);
+            }
+            treeView1.EndUpdate();
+        }
+
+
+
+        void webShow(string path)
+        {
+            txtAddress.Text = path;
+            string PATH = path.Substring(0, 3);
+            if (path.Length > 3)
+            {
+                path = path.Remove(0, 4);
+                path = PATH + path;
+            }
+            FULL_PATH = path;
+            webBrowser1.Url = new Uri(path); 
+        }
+
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            webShow(e.Node.FullPath);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                webBrowser1.GoForward();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(txtAddress.Text);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string fullPath;
+            fullPath = Path.GetFullPath(listBox1.SelectedItem.ToString());
+
+            webBrowser1.Url = new Uri(fullPath);
+            txtAddress.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SearchFile();
+        }
+
+        void SearchFile()
+        {
+            string[] dirs = Directory.GetFiles(FULL_PATH, comboBox1.Text);
+            listBox1.Items.Clear();
+            foreach (string dir in dirs)
+            {
+                listBox1.Items.Add(dir);
+            }
+        }
+    }
+}
